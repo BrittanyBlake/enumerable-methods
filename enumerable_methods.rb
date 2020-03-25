6938ffc53b9f4f1c644ded1105e63cd8255a370e
Brittany Blake <brittany.blake49@gmail.com>
2020-03-25 19:13:25 +0100

    Add select method

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -11,6 +11,7 @@ module Enumerable
     self
   end
   #end of my_each
+
    #start of my_each_with_index
   def my_each_with_index
     return to_enum unless block_given?
@@ -23,12 +24,28 @@ module Enumerable
     self
   end
   #end of my_each_with_index
+
+  #my_select
+  def my_select
+    return to_enum unless block_given?
+
+    new_array = []
+    i = 0
+
+    until i >= length
+      new_array << i if yield self[i]
+      i += 1
+    end
+  end
+  #end of my_select
 end
 
-
 #tests
- [1, 2, 3, 4, 5].each { |i| puts i }
- [1, 2, 3, 4, 5].my_each { |element| puts element }
+  [1, 2, 3, 4, 5].each { |i| puts i }
+  [1, 2, 3, 4, 5].my_each { |element| puts element }
 
-[1, 2, 3, 4, 5].each_with_index { |ele, idx|  puts "#{idx}:#{ele}" }
-[1, 2, 3, 4, 5].my_each_with_index { |ele, idx|  puts "#{idx}:#{ele}"}
+ [1, 2, 3, 4, 5].each_with_index { |ele, idx|  puts "#{idx}:#{ele}" }
+ [1, 2, 3, 4, 5].my_each_with_index { |ele, idx|  puts "#{idx}:#{ele}"}
+
+ [1, 2, 3, 4, 5].select { |i| p i.even? }
+[3, 3, 3, 4, 5].my_select { |i| p i.even? }
