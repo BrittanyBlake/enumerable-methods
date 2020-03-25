69ac14b2e9ca8a92f8d12f576eb1a4851a21903e
Brittany Blake <brittany.blake49@gmail.com>
2020-03-25 18:50:35 +0100

    Add  my_each_with_index method

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -11,8 +11,24 @@ module Enumerable
     self
   end
   #end of my_each
+   #start of my_each_with_index
+  def my_each_with_index
+    return to_enum unless block_given?
+
+    i = 0
+    until i >= length
+      yield self[i], i
+      i += 1
+    end
+    self
+  end
+  #end of my_each_with_index
 end
 
-[1, 2, 3, 4, 5].each { |i| puts i }
 
-[1, 2, 3, 4, 5].my_each { |element| puts element }
+#tests
+ [1, 2, 3, 4, 5].each { |i| puts i }
+ [1, 2, 3, 4, 5].my_each { |element| puts element }
+
+[1, 2, 3, 4, 5].each_with_index { |ele, idx|  puts "#{idx}:#{ele}" }
+[1, 2, 3, 4, 5].my_each_with_index { |ele, idx|  puts "#{idx}:#{ele}"}
