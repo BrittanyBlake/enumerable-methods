faceaf8181495cc30035284daacbf551df3179e6
Brittany Blake <brittany.blake49@gmail.com>
2020-05-01 14:55:13 +0200

    Add my_each_with_index to rspec

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -11,4 +11,11 @@ RSpec.describe Enumerable do
       expect([1, 2, 3, 4, 5].my_each { |num| puts num }).not_to eq([1, 2, 3, 4, 5, 6])
     end
   end
+
+  describe '#my_each_with_index' do
+    it 'should return each element of an array and its index' do
+      expect([1, 3, 2].each_with_index { |ele, idx| puts "#{idx}:#{ele}" }).to eq([1, 3, 2])
+      expect([1, 3, 2].each_with_index { |ele, idx| puts "#{idx}:#{ele}" }).not_to eq([1, 2, 3])
+    end
+  end
 end
